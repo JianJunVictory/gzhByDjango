71171ddc8fa6c8d4f63f1a8a8e4b221325bb4442
enjoyass@outlook.com <enjoyass@outlook.com>
2018-04-15 21:29:49 +0800

    modify config.py

--- wechat/wechatApi/config.py
+++ wechat/wechatApi/config.py
@@ -3,9 +3,10 @@ import os
 file=os.path.split(os.path.realpath(__file__))[0]+'\\wechat.txt'
 class Option(object):
     def __init__(self):
-        self.appId="wx2e0eb153224d114c"
-        self.appsecret="756ba0d940017ccd4658894cea69c630"
-        self.token="guangxinxi"
+        self.appId="自己的appID"
+        self.appsecret="自己的appsecret"
+        self.token="自己设置的token"
+        
     def getAccessToken(self):
         f=open(file,'r')
         accessToken=f.read()
